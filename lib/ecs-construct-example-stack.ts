97de3095725662360add4b0bcdf3eb6dcafd4c94
Clari Loo <clarizlook.tech@gmail.com>
2024-10-22 23:38:31 +0800

    Initial commit for the ecs construct

--- lib/ecs-construct-example-stack.ts
+++ lib/ecs-construct-example-stack.ts
@@ -1,16 +1,49 @@
 import * as cdk from 'aws-cdk-lib';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as ecr from 'aws-cdk-lib/aws-ecr';
+import * as elasticloadbalancing from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class EcsConstructExampleStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+interface ECSStackProps extends cdk.StackProps {
+  projectName: string;
+  environment: string;
+}
+
+export class EcsFargateStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: ECSStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const projectName = props.projectName;
+    const projectPrefix = `${projectName}-${props.environment}-server`;
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'EcsConstructExampleQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // We create a vpc for the ecs. We can also reference a vpc if it has been created prior this deployment
+    const vpc = new ec2.Vpc(this, 'Primary-Vpc', {
+      maxAzs: 2, 
+    });
+     
+    // Create an ECR repo to store images to be used by the ECS task definition
+    const repository = new ecr.Repository(this, `${projectPrefix}-repository`, {
+      repositoryName: `${projectPrefix}-repository`,
+    });
+
+    const cluster = new ecs.Cluster(this, `ECS-Cluster`, {
+      clusterName: `${projectPrefix}-cluster`,
+      vpc,
+    });
+
+     // load balancer resources
+     const elb = new elasticloadbalancing.ApplicationLoadBalancer(
+      this,
+      `LoadBalancer`,
+      {
+        vpc,
+        vpcSubnets: { subnets: vpc.publicSubnets },
+        internetFacing: true,
+      }
+    );
+
+
+
   }
 }
